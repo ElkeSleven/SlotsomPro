24b880aaa0359bc8a2e2c08fa6a1aa92c45bf287
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-17 01:46:29 +0200

    startpagina

---
+++ webstorm/viewStartPagina/script.js
@@ -0,0 +1,10 @@
+
+
+const viewlinks = [{
+    naam: 'ClassLibraryCsharp',
+    href:'../viewClassLibraryCsharp/viewClassLibraryCsharp.html',
+    reference: ['ClassLibrary','Csharp', 'namespaces', 'System']
+},{
+
+}
+]
