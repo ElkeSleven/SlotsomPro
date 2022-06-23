6a715783fb2fdf76c93b47b977e0c015ee6b207e
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-23 15:26:51 +0200

    update data links

--- webstorm/Data/Links.js
+++ webstorm/Data/Links.js
@@ -1,52 +1,63 @@
 
-
+/*
+development: 'fullstack',
+languages: ['css','react','js','vue','sql', 'mongodb','csharp'],
+* */
 
 // learn play
-
+// development: 'backend frontend fullstack',  languages: ['json', 'csv', ],
 const awesomeLinks = [
         {
+            development: 'frontend',
+            languages: ['css'],
             group: 'Website Link',
-            language: 'CSS',
             naam: 'fancy-border-radius',
             omschrijving: 'maak custom css figuren',
             url: "https://9elements.github.io/fancy-border-radius/",
-            reference: [ 'border-radius','Blob', 'Cirkel','Figuur','Figuren', 'Shape' ]
+            reference: [ 'border-radius','blob', 'cirkel','figuur','figuren', 'shape' ]
         },
         {
+
+            development: 'backend',
+            languages: ['json', 'csv'],
             group: 'Website Link',
-            language: 'All',
             naam: 'CSV-JSON',
             omschrijving: 'zet csv naar json of json naar csv',
             url: "https://csvjson.com/",
-            reference: [ 'JSON','csv' ]
+            reference: [ 'json','csv' ]
         },
         {
+
+            development: 'frontend',
+            languages: ['css'],
             group: 'Website Link',
-            language: 'CSS',
             naam: 'CSSBattle',
             omschrijving: 'test je css kennis',
             url: "https://cssbattle.dev/",
             reference: [ 'play', 'learn']
         },
         {
+            development: 'frontend',
+            languages: ['css'],
             group: 'Website Link',
-            language: 'CSS',
             naam: 'SVG assets designer ',
             omschrijving: 'Generate unique SVG design assets',
             url: "https://haikei.app/",
             reference: [ 'Wave', 'Background', 'Blob', 'Waves', 'Shape', 'Figuur','Figuren', 'SVG']
         },
         {
+            development: 'frontend',
+            languages: ['css'],
             group: 'Website Link',
-            language: 'CSS',
             naam: 'SVG shape divider',
             omschrijving: 'Generate SVG shape divider',
             url: "https://www.shapedivider.app/",
             reference: ['Wave', 'Dividers', 'Waves', 'Shape', 'Figuur','Figuren', 'SVG']
         },
         {
+            development: 'frontend',
+            languages: ['css'],
             group: 'Website Link',
-            language: 'All',
             naam: 'microsoft lean platform',
             omschrijving: 'leer modules van microsoft ',
             url: "https://docs.microsoft.com/en-us/learn/browse/",
@@ -54,119 +65,153 @@ const awesomeLinks = [
                 ,'Developer', 'DevOps','Visual Studio','.NET','Entity Framework', '.NET Core']
         },
         {
+            development: 'frontend',
+            languages: ['css'],
             group: 'Website Link',
-            language: 'CSS',
             naam: 'Conquering Responsive Layouts',
             omschrijving: 'van Kevin Powell(youtube) gratis cursus over responsive web-design',
             url: "https://kevinpowell.co/responsive",
             reference: ['responsive','layout', 'learn']
         },
         {
+            development: 'backend',
+            languages: ['csharp', 'js','typescript',],
             group: 'Website Link',
-            language: 'All',
-            naam: '',
-            omschrijving: '',
+            naam: 'LeetCode Questions',
+            omschrijving: 'website met links naar oefenigen  ',
             url: "https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU",
             reference: ['learn']
         },
         {
+            development: 'fullstack',
+            languages: ['csharp', 'js','typescript','sql'],
             group: 'Website Link',
-            language: 'All',
             naam: 'codewars',
-            omschrijving: 'wat is jou rank ?',
+            omschrijving: 'complete challenges to rank up',
             url: "https://www.codewars.com/",
             reference: ['learn','play']
         },
+    // hier beginnen de YT kanalen
         {
+            development: 'fullstack',
+            languages: ['css','react','js','vue','sql', 'mongodb','csharp'],
             group: 'YT-Channel',
-            language: 'All',
             naam: 'Fireship',
-            omschrijving: '',
+            omschrijving: 'code tutorials to help you build & ship your app faster.',
             url: "https://www.youtube.com/c/Fireship",
             reference: []
         },
         {
+
+        development: 'frontend',
+        languages: ['js','vue','react'],
+        group: 'YT-Channel',
+        naam: 'Tyler Potts',
+        omschrijving: '',
+        url: "https://www.youtube.com/c/TylerPotts",
+        reference: []
+    },
+        {
+            development: 'frontend',
+            languages: ['css','html','js','vue','node.js',],
             group: 'YT-Channel',
-            language: 'All',
             naam: 'dcode',
-            omschrijving: '',
+            omschrijving: 'web development tutorials',
             url: "https://www.youtube.com/c/dcode-software",
             reference: []
         },
         {
+            development: 'frontend',
+            languages: ['css','html','js',],
             group: 'YT-Channel',
-            language: 'JS',
             naam: 'Web Dev Simplified',
             omschrijving: '',
             url: "https://www.youtube.com/c/WebDevSimplified",
             reference: []
         },
         {
+            development: 'frontend',
+            languages: ['css','html','sass',],
             group: 'YT-Channel',
-            language: 'CSS',
             naam: 'Kevin Powell',
             omschrijving: '',
             url: "https://www.youtube.com/kepowob",
             reference: []
         },
         {
+
+            development: 'backend',
+            languages: ['csharp', 'sql'],
             group: 'YT-Channel',
-            language: 'Csharp',
             naam: 'IAmTimCorey',
-            omschrijving: '',
+            omschrijving: 'legt principes van Csharp uit',
             url: "https://www.youtube.com/user/IAmTimCorey",
             reference: [ 'WPF', 'xaml']
         },
         {
+            development: 'backend',
+            languages: ['csharp', 'sql'],
             group: 'YT-Channel',
-            language: 'Csharp',
             naam: 'Rainer Stropek',
-            omschrijving: '',
+            omschrijving: 'legt principes van Csharp uit <br> P.S. gebruikt nieuwst versie van .net',
             url: "https://www.youtube.com/c/RainerStropek11",
             reference: []
         },
         {
+            development: 'fullstack',
+            languages: ['csharp','js','html','css',],
             group: 'YT-Channel',
-            language: 'All',
             naam: 'Bro Code',
-            omschrijving: 'tutorials, programming examples (games)',
+            omschrijving: 'tutorials,  data structure (no code)  , games in JS ',
             url: "https://www.youtube.com/c/BroCodez",
             reference: []
         },
         {
+            development: 'fullstack',
+            languages: ['csharp',],
             group: 'YT-Channel',
-            language: 'Csharp',
             naam: 'Moo ICT',
-            omschrijving: '',
+            omschrijving: 'maakt games in enkel C# en xaml ',
             url: "https://www.youtube.com/c/MooICT",
             reference: [ 'WPF', 'xaml']
         },
         {
+            development: 'backend',
+            languages: ['csharp','.net'],
             group: 'YT-Channel',
-            language: 'Csharp',
             naam: 'dotNET',
-            omschrijving: '',
+            omschrijving: 'kanaal over .net ',
             url: "https://www.youtube.com/c/dotNET",
             reference: ['.Net']
         },
+    // hier beginnen de linken naar YT video's of afspeellijsten
         {
+            development: 'backend',
+            languages: ['csharp'],
             group: 'YT-Video',
-            language: 'Csharp',
             naam: 'C# for Beginners',
             omschrijving: '8uur uitleg van \'hello world\' tot lists, entities, ...',
             url: "https://www.youtube.com/watch?v=0QUgvfuKvWU",
             reference: ['ConsoleAPP', 'Beginner']
         },
         {
+            development: 'frontend',
+            languages: ['css'],
             group: 'YT-Video',
-            language: 'CSS',
             naam: 'CSS units : px , rem , em , vh , vw , ...',
             omschrijving: 'uitleg over CSS units',
             url: "https://www.youtube.com/watch?v=N5wpD9Ov_To",
             reference: ['units','rem','em']
-        },
+        },{
+        development: 'backend',
+        languages: ['csharp'],
+        group: 'YT-Video',
+        naam: 'C# for Beginners to pro',
+        omschrijving: 'uitleg over C# <br> van hello wold to linq en oop',
+        url: "https://www.youtube.com/watch?v=BM4CHBmAPh4&list=PLdo4fOcmZ0oVxKLQCHpiUWun7vlJJvUiN",
+        reference: ['linq', 'Pro1Sem1', 'Pro1Sem2']
+    }
 ]
-
 
 
 
